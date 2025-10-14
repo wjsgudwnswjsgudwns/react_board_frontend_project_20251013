9a0c750e737bb33d53917745c723c2f4286c3db9
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-14 17:43:40 +0900

    글 수정

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -13,11 +13,18 @@ function BoardDetail({user}) {
     const [error, setError] = useState("");
     const {id} = useParams();
 
+    const[editing,setEditing] = useState(false); // 수정화면 출력 여부
+    const[title,setTitle] = useState("");
+    const[content,setContent] = useState("");
+
+    //특정 글 보기
     const loadPost = async() => { // 특정 글 요청
         try{
             setLoading(true);
             const res = await api.get(`/api/board/${id}`);
             setPost(res.data);
+            setTitle(res.data.title); // 원본 글의 제목을 수정화면에 표시하는 변수인 title 변수에 저장
+            setContent(res.data.content); // 원본 글을 수정화면에 표시하는 변수인 content 변수에 저장
         } catch(err) {
             console.error(err);
             setError("존재하지 않는 게시글입니다.");
@@ -31,6 +38,7 @@ function BoardDetail({user}) {
         loadPost();
     },[id]);
 
+    //삭제
     const handleDelete = async() => {
         try{
             if(window.confirm("정말 삭제하시겠습니까?")) {
@@ -49,6 +57,27 @@ function BoardDetail({user}) {
         
     };
 
+    // 수정
+    const handleUpdate = async() => {
+        if(!window.confirm("정말 수정하시겠습니까?")) {
+            return;
+        }
+
+        try {
+            const res = await api.put(`/api/board/${id}`, {title, content});
+            alert("수정 되었습니다.");
+            setPost(res.data); // 새로 수정된 글로 post 변경
+            setEditing(false);
+        } catch(err) {
+            console.error(err);
+            if(err.response.state === 403) {
+                alert("수정 권한이 없습니다.");
+            } else {
+                alert("수정 실패 했습니다.")
+            }
+        }
+    }
+
     if(loading) return <p>게시글 불러오는 중</p>
     if(error) return <p style={{color:"red"}}>{error}</p>
     if(!post) return <p style={{color:"red"}}>존재하지 않는 글입니다.</p>
@@ -59,19 +88,37 @@ function BoardDetail({user}) {
     return (
 
         <div className="detail-container">
-            <h2>{post.title}</h2>
-            <p className="author">작성자 : {post.author.username}</p>
-            <div className="content">{post.content}</div>
 
-            <div className="button-group">
-                <button onClick={() => navigate("/board")}>목록</button>
-                { isAuthor && (
+            {editing ? (
+                <div className="edit-form">
+                    <h2>글 수정</h2>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                    <textarea value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                    <div className="button-group">
+                        <button className="edit-button" onClick={handleUpdate}>저장</button>
+                        <button className="delete-button" onClick={() => setEditing(false)}>취소</button>
+                    </div>
+                </div>                    
+            ) : 
+            
+            (
                 <>
-                    {<button className="edit-button">수정</button>}
-                    {<button className="delete-button" onClick={handleDelete}>삭제</button>}
+                    <h2>{post.title}</h2>
+                    <p className="author">작성자 : {post.author.username}</p>
+                    <div className="content">{post.content}</div>
+
+                    <div className="button-group">
+                        <button onClick={() => navigate("/board")} className="list-button">목록</button>
+                        { isAuthor && (
+                        <>
+                            {<button className="edit-button" onClick={() => setEditing(true)}>수정</button>}
+                            {<button className="delete-button" onClick={handleDelete}>삭제</button>}
+                        </>
+                        )}
+                    </div>
                 </>
-                )}
-            </div>
+            )}
+
         </div>
     );
 }
